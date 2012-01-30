03b8c8caed55d97eec269f23092b78aaffd1fc15
Shannon Sun <shannonsun@Shannon-Suns-MacBook-Pro.local>
2012-01-29 22:52:14 -0500

    added in the pages view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Sugarcube::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
