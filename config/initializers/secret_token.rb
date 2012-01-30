ca1407caa877428185ccf5c129c3e682acbc9e59
Shannon Sun <shannonsun@Shannon-Suns-MacBook-Pro.local>
2012-01-29 21:49:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sugarcube::Application.config.secret_token = '3937b49114b0f6b8d14661a4d0113cb54343da906d8539ec8ed674da0ef8515ccc418ede16747d8c41cd5fb5549befc04da5f55e2f9a21a0c3c52f2c4b94f988'
