2efd5831bc328c27d77ce1d36e51f732c3183289
Shannon Sun <shanonsun@gmail.com>
2012-03-05 14:38:12 -0500

    added in the password fields in signup and took out the duplicated header

---
+++ db/migrate/20120305193102_add_password_confirmation_to_users.rb
@@ -0,0 +1,6 @@
+class AddPasswordConfirmationToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password_confirmation, :string
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120227224438) do
+ActiveRecord::Schema.define(:version => 20120305193102) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(:version => 20120227224438) do
     t.string   "shortest_relationship"
     t.string   "longest_relationship"
     t.string   "salt"
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
