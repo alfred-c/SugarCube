9a413fb382ca31fa8dccdb897f311d2495d8ca78
alfred-c <alfred_c@hotmail.com>
2012-04-08 23:15:30 -0500

    content belongs to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120408185206) do
+ActiveRecord::Schema.define(:version => 20120409040422) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20120408185206) do
     t.string   "type"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "friendships", :force => true do |t|
