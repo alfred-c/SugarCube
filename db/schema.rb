9340040a40776551fee99367571082615dad8273
Shannon Sun <shanonsun@gmail.com>
2012-09-16 23:26:28 -0400

    - Added in the title column for journal entry table - being able to show "created at" and "title' for journal entry when it's been created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120911022426) do
+ActiveRecord::Schema.define(:version => 20120917024150) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(:version => 20120911022426) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.integer  "forum_id"
+    t.string   "title"
   end
 
   create_table "users", :force => true do |t|
